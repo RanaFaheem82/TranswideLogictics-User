f04d082f6f78fdb7763217935cf751e21ca63534
usman bajwa <usmanbajwa878@gmail.com>
2020-02-18 17:26:07 +0500

    Initial Design

--- TranswideLogisticsUser/TranswideLogisticsUser/Global/Extensions/Extension.swift
+++ TranswideLogisticsUser/TranswideLogisticsUser/Global/Extensions/Extension.swift
@@ -57,7 +57,9 @@ extension UIView {
        class func fromNib<T : UIView>() -> T {
            return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
        }
+    
 }
+
 
 extension UITextField {
     @IBInspectable var textInsets: CGPoint {
